a9a6e7762a39f471da6b12c404d10a3898ceaf5f
asidikfauzi <asidikfauzi@gmail.com>
2023-02-24 17:01:20 +0700

    jwtguards

---
+++ src/guards/auth.guard.ts
@@ -0,0 +1,22 @@
+import { ExecutionContext, Injectable, UnauthorizedException } from "@nestjs/common";
+import { TokenService } from "src/services";
+
+@Injectable()
+export class JwtAuthGuard {
+    constructor(private tokenService: TokenService) {}
+
+    async canActive(context: ExecutionContext) {
+        const request = context.switchToHttp().getRequest();
+        let token = request.headers['authorization'];
+        if(!token) {
+            throw new UnauthorizedException();
+        }
+        token = token.replace('Bearer ', '');
+        const user = await this.tokenService.validateToken(token);
+        if(!user) {
+            throw new UnauthorizedException();
+        }
+        request.user = user;
+        return true;
+    }
+}
