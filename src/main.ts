98a9e4d22f0bd83e601482c39ae2d4f5adc8813f
asidikfauzi <asidikfauzi@gmail.com>
2023-02-24 22:33:07 +0700

    connect microservice rbmq

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,30 @@
+import { ValidationPipe } from '@nestjs/common';
 import { ConfigService} from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
+import { Transport } from '@nestjs/microservices';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
   
+  app.useGlobalPipes(new ValidationPipe());
+
+  app.connectMicroservice({
+    transport: Transport.RMQ,
+    options: {
+      urls: [configService.get('RBMQ_URL')],
+      queue: configService.get('RBMQ_AUTH_QUEUE'),
+      queueOptions: { durable: false },
+      prefetchCount: 1,
+    },
+  });
+
+  await app.startAllMicroservices();
+  
   const port = configService.get('PORT')
   await app.listen(port);
+  console.log(`Authentication service running on port ${port}`)
 }
 bootstrap();
 
