9c27142f9078621426e06a21b9f9ad0d34fe88ff
asidikfauzi <asidikfauzi@gmail.com>
2023-02-24 14:37:29 +0700

    setup modul RBMQ

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,31 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { PrismaService, TokenService } from './services';
+import { ClientsModule, Transport } from '@nestjs/microservices';
 
 @Module({
-  imports: [ConfigModule.forRoot()],
+  imports: [
+    ConfigModule.forRoot(),
+    ClientsModule.registerAsync([
+      {
+        name: 'LOG_SERVICE',
+        imports: [ConfigModule],
+        useFactory: async(configService: ConfigService) => ({
+          transport: Transport.RMQ,
+          options: {
+            urls: [`${configService.get('RBMQ_URL')}`],
+            queue: `${configService.get('RBMQ_LOG_QUEUE')}`,
+            queueOptions: {
+              durable: false,
+            },
+          },
+        }),
+        inject: [ConfigService],
+      }
+    ])
+  ],
   controllers: [AppController],
   providers: [AppService, PrismaService, TokenService],
 })
