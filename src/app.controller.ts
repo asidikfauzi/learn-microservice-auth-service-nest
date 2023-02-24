a682e4d86c363f1b34dfce65ec44d865ced11cf6
asidikfauzi <asidikfauzi@gmail.com>
2023-02-24 15:03:43 +0700

    register

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,7 +1,6 @@
-import { Body, Controller, Get, HttpCode, Post, Req } from '@nestjs/common';
+import { Body, Controller, Get, HttpCode, Post, Req, Ip } from '@nestjs/common';
 import { AppService } from './app.service';
-import { LoginDTO } from './dtos/login.dto';
-import { RegisterDTO } from './dtos/register.dto';
+import { LoginDTO, RegisterDTO } from './dtos';
 
 @Controller()
 export class AppController {
@@ -10,12 +9,12 @@ export class AppController {
   @Post()
   @HttpCode(200)
   async login(@Req() req: Request, @Body() body: LoginDTO) {
-
+    return this.appService.login(req, body)
   }
 
   @Post('register')
   @HttpCode(201)
-  async register(@Req() req: Request, @Body() body: RegisterDTO) {
-
+  async register(@Req() req: Request, @Body() body: RegisterDTO, @Ip() ip) {
+    return this.appService.register(req, body, ip)
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,130 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, HttpStatus, Inject, Injectable, NotFoundException } from '@nestjs/common';
+import { ClientProxy } from '@nestjs/microservices';
+import { User } from '@prisma/client';
+import { LoginDTO, RegisterDTO } from './dtos';
+import { PrismaService, TokenService } from './services';
+import * as bcrypt from 'bcrypt'
 
 @Injectable()
 export class AppService {
   constructor(
-    // private prisma: 
+    private prisma: PrismaService,
+    private token: TokenService,
+    @Inject('LOG_SERVICE')
+    private readonly logClient: ClientProxy,
   ) {}
+
+  async login(req, body: LoginDTO) {
+    const {username_or_email, password} = body;
+
+    const user = await this.prisma.user.findFirst({
+      include: {
+        role: {
+          include: {
+            permissions: {
+              include: {
+                permission: true,
+              },
+            },
+          },
+        },
+      },
+      where: {
+        OR: [
+          {email:username_or_email},
+          {username:username_or_email},
+        ]
+      }
+    });
+
+    if(!user) {
+      return new NotFoundException(
+        "We couldn't find your account. Please check your information or sign up for a new account."
+      )
+    }
+  }
+
+  async register(req, body: RegisterDTO, ip) {
+    const {username, email, password, role_id} = body
+
+    const role = await this.prisma.role.findUnique({
+      where: {id: role_id},
+      include: {
+        permissions: {
+          include: {
+            permission:true,
+          }
+        }
+      }
+    })
+     
+    if(!role) {
+      return new BadRequestException('Invalid role id')
+    }
+    
+    //check username exist
+    const usernameExist = await this.prisma.user.findUnique({
+      where: {username: username}
+    })
+
+    if(usernameExist) {
+      return new BadRequestException('Username already exist.')
+    }
+
+    //check email exist
+    const emailExist = await this.prisma.user.findUnique({
+      where: {email:email}
+    })
+
+    if(emailExist) {
+      return new BadRequestException('Email already exist.')
+    }
+
+    const newUser = {} as User;
+    newUser.username = username;
+    newUser.email = email;
+    newUser.role_id = role_id;
+    newUser.is_active = true;
+    newUser.password = await bcrypt.hash(password,10)
+
+    const user = await this.prisma.user.create({data: newUser})
+
+    const permissionCodes = role.permissions.map(
+      ({ permission }) => permission.permission_code
+    );
+
+    const accesToken = await this.token.generateToken(
+      user.id,
+      role.role_name,
+      permissionCodes,
+    );
+
+    user['role_name'] = role.role_name;
+    delete user.role_id;
+    delete user.password;
+    delete user.deleted_at;
+
+    const getIp = ip.replace(/:|f/g, "");
+    const userAgent = req.headers['user-agent'];
+
+    const logPayload = {
+      timestamp: new Date(),
+      action: 'REGISTER',
+      user_id: user.id,
+      ip_address: getIp,
+      user_agent: userAgent,
+      details: '',
+    };
+
+    this.logClient.emit('add_log', JSON.stringify(logPayload));
+
+    return {
+      statusCode: HttpStatus.OK,
+      message: 'Successfully Register',
+      data : {
+        accesToken,
+        user,
+      },
+    };
+  }
 }
